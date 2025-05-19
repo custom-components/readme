e15e99fb1b04220bf76ab66995740c389a7f1717
Joakim Sørensen <joasoe@proton.me>
2025-05-19 10:52:54 +0200

    Remove startup message (#30)

--- custom_components/readme/const.py
+++ custom_components/readme/const.py
@@ -6,17 +6,5 @@ LOGGER: logging.Logger = logging.getLogger(__package__)
 
 DOMAIN = "readme"
 DOMAIN_DATA = "readme_data"
-INTEGRATION_VERSION = "0.6.0"
 
 ISSUE_URL = "https://github.com/custom-components/readme/issues"
-
-
-STARTUP_MESSAGE = f"""
--------------------------------------------------------------------
-{DOMAIN}
-Version: {INTEGRATION_VERSION}
-This is a custom integration
-If you have any issues with this you need to open an issue here:
-{ISSUE_URL}
--------------------------------------------------------------------
-"""
